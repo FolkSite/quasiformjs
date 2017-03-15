2e6b1d888513fc47ffe3f02d5eea4df490e1c226
misha8000 <quasi-art@ya.ru>
2017-03-16 01:14:39 +0300

    First commit.

---
+++ test/main.js
@@ -0,0 +1,52 @@
+$(function () {
+    /**
+     * Нулевая форма
+     */
+    let optionsPost = {
+        debug: true,
+        hasErrorInputClass: 'quasiform-form__input--has-error',
+        hasErrorLabelClass: 'quasiform-form__label--has-error',
+        hideFormOnSuccess: false,
+        callbackOnSuccess: function callbackOnSuccess(wrapper) {
+            console.debug(wrapper);
+        },
+        callbackOnFail: function callbackOnFail(wrapper) {
+            console.debug(wrapper);
+        },
+    };
+    let quasiformPost = $('#post').quasiform(optionsPost);
+	
+    /**
+     * Первая форма
+     */
+    let optionsSuccess = {
+        debug: true,
+        hasErrorInputClass: 'quasiform-form__input--has-error',
+        hasErrorLabelClass: 'quasiform-form__label--has-error',
+        hideFormOnSuccess: false,
+        callbackOnSuccess: function callbackOnSuccess(wrapper) {
+            console.debug(wrapper);
+        },
+        callbackOnFail: function callbackOnFail(wrapper) {
+            console.debug(wrapper);
+        },
+    };
+    let quasiformSuccess = $('#success').quasiform(optionsSuccess);
+
+    /**
+     * Вторая форма
+     */
+    let optionsFail = {
+        debug: true,
+        hasErrorInputClass: 'quasiform-form__input--has-error',
+        hasErrorLabelClass: 'quasiform-form__label--has-error',
+        hideFormOnSuccess: false,
+        callbackOnSuccess: function callbackOnSuccess(wrapper) {
+            console.debug(wrapper);
+        },
+        callbackOnFail: function callbackOnFail(wrapper) {
+            console.debug(wrapper);
+        },
+    };
+    let quasiformFail = $('#fail').quasiform(optionsFail);
+});
