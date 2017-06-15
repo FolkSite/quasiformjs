c8b7e5ec671e3225847fdf19f07b16549ead286c
misha8000 <quasi-art@ya.ru>
2017-06-15 21:49:58 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: [/*'babel-polyfill',*/ /*'whatwg-fetch', */ 'fetch-xhr', './test/main.js'],
+    entry: ['fetch-xhr', './test/main.js'],
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist/js'),
@@ -23,15 +23,11 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"production"'
         }),
-        //new BundleAnalyzerPlugin(),
-        /*new webpack.ProvidePlugin({
-            Promise: 'imports-loader?this=>global!exports-loader?global.Promise!es6-promise',
-            fetch: 'imports-loader?this=>global!exports-loader?global.fetch!fetch'
-        }),*/
+        /*new BundleAnalyzerPlugin(),*/
         new UglifyJSPlugin(),
     ],
     stats: {
         colors: true
     },
-    devtool: 'source-map'
+    //devtool: 'source-map'
 };
