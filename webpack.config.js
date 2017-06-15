885fc3e1389ff319d57993248689b0ac037ec7ec
misha8000 <quasi-art@ya.ru>
2017-06-15 05:51:25 +0300

    Some code

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,11 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    //entry: './src/js/quasiform.js',
     entry: ['babel-polyfill', 'whatwg-fetch', './test/main.js'],
     target: 'web',
     output: {
         path: path.resolve(__dirname, 'dist/js'),
-        filename: 'quasiform.min.js'
+        filename: 'bundle.min.js'
     },
     module: {
         loaders: [{
